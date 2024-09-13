1c92f32137d9cf06f13ae15a58f3a12f5e0d8f33
lewisxhe <lewishe@outlook.com>
2024-09-13 11:39:46 +0800

    Update lv config

--- lib/lv_conf.h
+++ lib/lv_conf.h
@@ -268,7 +268,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define lv_snprintf  snprintf
 #define lv_vsnprintf vsnprintf
 #else   /*LV_SPRINTF_CUSTOM*/
-#define LV_SPRINTF_USE_FLOAT 0
+#define LV_SPRINTF_USE_FLOAT 1
 #endif  /*LV_SPRINTF_CUSTOM*/
 
 #define LV_USE_USER_DATA 1
@@ -667,7 +667,7 @@ e.g. "stm32f769xx.h" or "stm32f429xx.h"*/
 #define LV_USE_IMGFONT 0
 
 /*1: Enable a published subscriber based messaging system */
-#define LV_USE_MSG 0
+#define LV_USE_MSG 1
 
 /*==================
 * EXAMPLES

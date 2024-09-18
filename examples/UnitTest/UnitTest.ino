993ee80cab4ff8baeb27dd03fc048e5ebd9cd67b
lewisxhe <lewishe@outlook.com>
2024-09-18 14:05:14 +0800

    Fix Arduino IDE compilation

--- examples/UnitTest/UnitTest.ino
+++ examples/UnitTest/UnitTest.ino
@@ -33,12 +33,11 @@
 #define USE_ESP_VAD
 
 #include "utilities.h"
-
+#include "config.h"
 
 #ifndef BOARD_HAS_PSRAM
 #error "Detected that PSRAM is not turned on. Please set PSRAM to OPI PSRAM in ArduinoIDE"
 #endif
-
 
 #ifndef SerialGPS
 #define SerialGPS Serial1
@@ -54,14 +53,6 @@ TinyGPSPlus gps;
 #define VAD_SAMPLE_RATE_HZ          16000
 #define VAD_FRAME_LENGTH_MS         30
 #define VAD_BUFFER_LENGTH           (VAD_FRAME_LENGTH_MS * VAD_SAMPLE_RATE_HZ / 1000)
-
-LV_IMG_DECLARE(image_output);
-LV_IMG_DECLARE(image);
-LV_IMG_DECLARE(image1);
-LV_IMG_DECLARE(image2);
-LV_IMG_DECLARE(image3);
-LV_IMG_DECLARE(image4);
-
 
 #if TFT_DC !=  BOARD_TFT_DC || TFT_CS !=  BOARD_TFT_CS || TFT_MOSI !=  BOARD_SPI_MOSI || TFT_SCLK !=  BOARD_SPI_SCK
 #error "Not using the already configured T-Deck file, please remove <Arduino/libraries/TFT_eSPI> and replace with <lib/TFT_eSPI>, please do not click the upgrade library button when opening sketches in ArduinoIDE versions 2.0 and above, otherwise the original configuration file will be replaced !!!"
@@ -149,17 +140,7 @@ uint32_t    last_update_millis = 0;
 lv_obj_t    *main_count;
 
 
-#ifndef WIFI_SSID
-#define WIFI_SSID             "You WIFI SSID"
-#endif
 
-#ifndef WIFI_PASSWORD
-#define WIFI_PASSWORD             "You WIFI PASSWORD"
-#endif
-
-#define NTP_SERVER1           "pool.ntp.org"
-#define NTP_SERVER2           "time.nist.gov"
-#define DEFAULT_TIMEZONE      "CST-8"         //When the time zone cannot be obtained, the default time zone is used
 
 
 static void setupLvgl();
---
+++ examples/UnitTest/config.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+#ifndef WIFI_SSID
+#define WIFI_SSID             "You WIFI SSID"
+#endif
+
+#ifndef WIFI_PASSWORD
+#define WIFI_PASSWORD         "You WIFI PASSWORD"
+#endif
+
+#define NTP_SERVER1           "pool.ntp.org"
+#define NTP_SERVER2           "time.nist.gov"
+#define DEFAULT_TIMEZONE      "CST-8"         //When the time zone cannot be obtained, the default time zone is used
+
+
+
